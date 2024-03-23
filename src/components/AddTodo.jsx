861a6405fe4c2d4aa1c4013991cc62b8ff41ce91
mewpk <milzaza123@gmail.com>
2024-03-23 20:03:27 +0700

    Refactor AddTodo component form layout

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -25,19 +25,29 @@ function AddTodo({ onSubmit, edit  , display}) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="todo-form flex justify-center mb-4">
-      <input
-        autoFocus
-        type="text"
-        placeholder="Add a todo"
-        value={input}
-        name="text"
-        className="todo-input border-2 border-gray-300 bg-white h-10 px-5 rounded-lg text-sm focus:outline-none"
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={handleSubmit} className="todo-button ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        {edit ? 'Update' : 'Add'}
-      </button>
+    <form 
+      onSubmit={handleSubmit} 
+      className={`${
+        edit ? "fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" : "mb-4"
+      } flex justify-center w-full`}
+    >
+      <div className={`${edit ? "bg-white p-4 shadow-lg rounded-lg" : ""}`}>
+        <input
+          type="text"
+          placeholder="Add a todo"
+          value={input}
+          autoFocus // This prop automatically focuses the input element when rendered
+          name="text"
+          className="border-2 border-gray-300 bg-white h-10 px-5 rounded-lg text-sm focus:outline-none"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button 
+          type="submit" 
+          className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          {edit ? 'Update' : 'Add'}
+        </button>
+      </div>
     </form>
   );
 }
